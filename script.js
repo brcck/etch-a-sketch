da5edc1329095c98db96dfcdac9a4a4a0b2a732b
Brock McElroy <28877984+brockt0pus@users.noreply.github.com>
2017-11-08 21:10:47 -0700

    Blocks change color on mouseover.

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 'esversion: 6';
 'use strict';
 
+function changeColor(e) {
+  e.currentTarget.classList.add("hover");
+}
+
+function resetColor(e) {
+  e.currentTarget.classList.remove("hover");
+}
+
 let container = document.querySelector('div');
-for (let i = 0; i <= 16; i++){
-  container.appendChild(document.createElement('div'));
+
+for (let i = 0; i <= 256; i++) {
+  let div = document.createElement('div');
+  div.id = i;
+  div.addEventListener("mouseover", changeColor);
+  // div.addEventListener("mouseout", resetColor);
+  container.appendChild(div);
 }
